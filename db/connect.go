1cecdd94eee06a7ddcf90410264de7539e20dbde
Lorenc326 <iLorenc326@gmail.com>
2020-11-25 01:38:14 +0200

    Enhance error handling, insert User on start

--- db/connect.go
+++ db/connect.go
@@ -5,18 +5,39 @@ import (
 	"github.com/go-pg/pg/v10"
 )
 
-func ConnectDB (url string) (*pg.DB, error) {
+type tableInitiator interface {
+	initiateTable(db *pg.DB) error
+}
+
+var models = []tableInitiator{
+	(*User)(nil),
+}
+
+func ConnectDB(url string) *pg.DB {
 	opt, err := pg.ParseURL(url)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
 
 	db := pg.Connect(opt)
-	defer db.Close()
+	defer func() {
+		// close connection and pass error next
+		if err := recover(); err != nil {
+			db.Close()
+			panic(err)
+		}
+	}()
 
 	ctx := context.Background()
 	if err := db.Ping(ctx); err != nil {
-		return nil, err
+		panic(err)
 	}
-	return db, nil
+
+	for _, model := range models {
+		if err := model.initiateTable(db); err != nil {
+			panic(err)
+		}
+	}
+
+	return db
 }
--- server.go
+++ server.go
@@ -9,30 +9,40 @@ import (
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
+func handleFatal() {
+	if err := recover(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	defer handleFatal()
+
 	bot, err := tb.NewBot(tb.Settings{
-		Token: config.botToken,
+		Token:  config.botToken,
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	db := dbUtils.ConnectDB(config.postgresUrl)
+	defer db.Close()
+
 	bot.Handle("/start", func(m *tb.Message) {
 		if !m.Private() {
 			return
 		}
+
+		user := &dbUtils.User{ID: m.Sender.ID, Lng: m.Sender.LanguageCode, CreatedAt: time.Now().Format(time.RFC3339)}
+		db.Model(user).SelectOrInsert()
+
 		bot.Send(m.Sender, "Hello there!\nI'll make sure your \U0001F9B7 are washed in time, just set your wake up/sleep hours. (it works better then alarm, trust me)")
 	})
 
 	bot.Handle(tb.OnText, func(m *tb.Message) {
-		log.Println(m.Text)
+		log.Println(m.Text, m.Sender, m.Sender.Username)
 	})
-
-	_, err = dbUtils.ConnectDB(config.postgresUrl)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	bot.Start()
 }
