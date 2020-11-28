54114d9f5381f52d0f915adf9642a45e03f75285
Lorenc326 <iLorenc326@gmail.com>
2020-11-28 22:44:39 +0200

    Validate input in time message handler

--- messages/time.go
+++ messages/time.go
@@ -11,26 +11,44 @@ import (
 )
 
 const timeMessage = "👍"
+const errorTimeMessage = "⚠️Invalid input ⚠\nTry such format \"hh:mm hh:mm GMT Timezone\", for example \"09:00 21:00 +02\"."
+
+func debugTimeHandler(m *tb.Message, bot *tb.Bot) {
+	if err := recover(); err != nil {
+		bot.Send(m.Sender, errorTimeMessage)
+	}
+}
 
 func GetTimeHandler(db *pg.DB, bot *tb.Bot) func(_ *tb.Message) {
 	return func(m *tb.Message) {
 		if !m.Private() {
 			return
 		}
+		defer debugTimeHandler(m, bot)
 
 		reg := regexp.MustCompile("[\\s\\,]+")
 		// Expected payload in "10:00 21:00 +2" format
 		times := reg.Split(strings.TrimSpace(m.Payload), 3)
+		if len(times) != 3 {
+			bot.Send(m.Sender, errorTimeMessage)
+			return
+		}
+
 		zoneOffset := times[2]
 		layout := "15:04 MST -07"
-		start, _ := time.Parse(layout, fmt.Sprintf("%s GMT %s", times[0], zoneOffset))
-		end, _ := time.Parse(layout, fmt.Sprintf("%s GMT %s", times[1], zoneOffset))
+		start, err1 := time.Parse(layout, fmt.Sprintf("%s GMT %s", times[0], zoneOffset))
+		end, err2 := time.Parse(layout, fmt.Sprintf("%s GMT %s", times[1], zoneOffset))
+		if err1 != nil || err2 != nil || start.After(end) {
+			bot.Send(m.Sender, errorTimeMessage)
+			return
+		}
 
 		user := &orm.User{
 			ID:          m.Sender.ID,
 			MorningTime: start.String(),
 			EveningTime: end.String(),
 		}
+		// TODO: do smth in case of failed save
 		user.SetReminders(db)
 
 		bot.Send(m.Sender, timeMessage)
