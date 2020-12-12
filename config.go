d7e1500c03eef68e0cff8cf6ff1bdb0ce92da632
Lorenc326 <iLorenc326@gmail.com>
2020-12-13 01:18:29 +0200

    Enhance config logic so it can receive vars directly from process

--- config.go
+++ config.go
@@ -7,16 +7,20 @@ import (
 )
 
 type EnvConfig struct {
-	botToken string
+	botToken    string
 	postgresUrl string
 }
 
 var config = EnvConfig{}
 
 func init() {
-	if err:= godotenv.Load(".env"); err != nil {
-		log.Fatal(err)
-	}
+	// in case vars are provided with file and not in process.env
+	godotenv.Load(".env")
+
 	config.botToken = os.Getenv("bot_token")
 	config.postgresUrl = os.Getenv("postgres_url")
+
+	if config.botToken == "" || config.postgresUrl == "" {
+		log.Fatal("Missing env variables")
+	}
 }
